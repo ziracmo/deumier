06a4488a5c592e71e37f6880d07c74d4ac151d41
AzSiAz <slider.perso@live.fr>
2018-11-15 15:12:13 +0100

    add profile test

--- imports/api/remote/users.js
+++
@@ -1,8 +0,0 @@
-// Remote Collection definition
-
-import { Meteor } from 'meteor/meteor';
-import Remote from './ddp';
-
-const Users = new Meteor.Collection('users', { connection: Remote });
-
-export default Users;
--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -2,14 +2,16 @@
  * Accounts Setup
  */
 
-import {Accounts} from 'meteor/accounts-base';
-import Counters from '../../api/counters/counters.js';
+import { Accounts } from 'meteor/accounts-base';
 
 Accounts.onCreateUser((options, user) => {
-    // init counter at 0
-    Counters.insert({
-        _id: user._id,
-        count: Number(0),
-    });
-    return user;
+	// init counter at 0
+	user.profile = {
+		nom: options.profile.nom,
+		prenom: options.profile.prenom,
+		gender: options.profile.gender,
+		birthdate: options.profile.birthdate,
+		phone: options.profile.phone
+	};
+	return user;
 });
